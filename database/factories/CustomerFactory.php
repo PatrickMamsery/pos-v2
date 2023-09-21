c5607ebec208159bd2073a56e33e4381399970c4
PatrickMamsery <pkrobert1612@gmail.com>
2023-09-21 11:48:43 +0300

    feat: add customer migrations and model

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'fname' => $this->faker->firstName(),
+            'lname' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'avatar' => $this->faker->imageUrl(640, 480, 'people', true),
+        ];
+    }
+}
